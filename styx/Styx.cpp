346f731f9f52ed0bb5d257a1e72cf5e7f1dfc670
jdlogan <jdlogan@mit.edu>
2015-01-27 16:24:29 -0500

    Attempt at sprites; crashes during draw()

--- styx/Styx.cpp
+++ styx/Styx.cpp
@@ -1,6 +1,29 @@
 
 #include "SFML/Graphics.hpp"
+#include <string>
+#include <cstring>
 
+
+//A sprite class for sprites of arbitrary size/position/texture
+
+
+class sprite{
+    public:
+        sf::Texture tex;
+    public:
+        sf::Sprite psprite;
+    public:
+        void init(std::string s, float xpos, float ypos){
+        tex.loadFromFile(s);
+        psprite.setTexture(tex);
+        psprite.setTextureRect(sf::IntRect(10,10,200,200));
+        psprite.setColor(sf::Color(200,100,210,150));
+        psprite.setPosition(xpos,ypos);
+        }
+
+};
+
+//The primary Game class, including graphics, event manager, game logic, etc
 class Game{
     public:
             Game();
@@ -13,8 +36,10 @@ class Game{
     private:
         sf::RenderWindow mWindow;
         sf::CircleShape mPlayer;
-
+        sprite* sPlayer;
 };
+
+
 Game::Game():mWindow(sf::VideoMode(640,480),"Styx"),mPlayer(){
     //initializes window with cyan circle
     mPlayer.setRadius(40.f);
@@ -23,7 +48,14 @@ Game::Game():mWindow(sf::VideoMode(640,480),"Styx"),mPlayer(){
 }
 
 void Game::run(){
+    //Run once
+    float xpos = 0;
+    float ypos = 0;
+    sprite* sPlayer = new sprite;
+    sPlayer->init("C:\\Users\\-\\Desktop\\Classes\\6.179\\Project\\Styx\\Styx\\Assets\\floating_eyebeast.png",xpos,ypos);
 
+
+    //Run continuously
     while(mWindow.isOpen()){
         processEvents();
         update();
@@ -31,7 +63,7 @@ void Game::run(){
     }
 }
 
-void Game::processEvents(){
+void Game::processEvents(){sf::Texture texture;
     //handles user input
     sf::Event event;
     while(mWindow.pollEvent(event)){
@@ -50,13 +82,17 @@ void Game::render(){
     //clears window first
     mWindow.clear();
     //draw all the objects of the current frame by calling draw method
-    mWindow.draw(mPlayer);
+    //mWindow.draw(mPlayer);
+    mWindow.draw(sPlayer->psprite);
     //after drawing everything, display it on the screen
     mWindow.display();
+
 }
 
 int main()
 {
+
+
     Game game;
     game.run();
 }
