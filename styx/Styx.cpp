3ecde803e951444dd92fb15b77dfee1655d9741e
Erwin Hilton <ehilton8@mit.edu>
2015-01-27 14:21:25 -0500

    clock/movement

--- styx/Styx.cpp
+++ styx/Styx.cpp
@@ -8,11 +8,16 @@ class Game{
 
     private:
         void processEvents();
-        void update();
+        void handlePlayerInput(sf::Keyboard::Key key, bool isPressed);
+        void update(sf::Time deltaTime);
         void render();
     private:
         sf::RenderWindow mWindow;
         sf::CircleShape mPlayer;
+        bool mIsMovingUp = false;
+        bool mIsMovingDown = false;
+        bool mIsMovingRight = false;
+        bool mIsMovingLeft = false;
 
 };
 Game::Game():mWindow(sf::VideoMode(640,480),"Styx"),mPlayer(){
@@ -23,28 +28,61 @@ Game::Game():mWindow(sf::VideoMode(640,480),"Styx"),mPlayer(){
 }
 
 void Game::run(){
-
+    sf::Clock clock;
     while(mWindow.isOpen()){
+        sf::Time deltaTime = clock.restart();
         processEvents();
-        update();
+        update(deltaTime);
         render();
     }
+}
+
+
+void Game::update(sf::Time deltaTime){
+    //updates game logic
+    float playerSpeed = 100.f;
+    sf::Vector2f movement(0.f,0.f);
+    if(mIsMovingUp){
+        movement.y -= playerSpeed;
+    }if(mIsMovingDown){
+        movement.y += playerSpeed;
+    }if(mIsMovingLeft){
+        movement.x -= playerSpeed;
+    }if(mIsMovingRight){
+        movement.x += playerSpeed;
+    }
+    mPlayer.move(movement*deltaTime.asSeconds());
 }
 
 void Game::processEvents(){
     //handles user input
     sf::Event event;
     while(mWindow.pollEvent(event)){
-        if(event.type == sf::Event::Closed){
-            mWindow.close();
+        switch(event.type){
+            case sf::Event::KeyPressed:
+                handlePlayerInput(event.key.code,true);
+                break;
+            case sf::Event::KeyReleased:
+                handlePlayerInput(event.key.code,false);
+                break;
+            case sf::Event::Closed:
+                mWindow.close();
+                break;
         }
     }
 }
 
-void Game::update(){
-    //empty for now, this updates game logic
+void Game::handlePlayerInput(sf::Keyboard::Key key, bool isPressed){
+    if (key == sf::Keyboard::W){
+        mIsMovingUp = isPressed;
+    }else if (key == sf::Keyboard::S){
+        mIsMovingDown = isPressed;
+    }else if(key == sf::Keyboard::A){
+        mIsMovingLeft = isPressed;
+    }else if(key == sf::Keyboard::D){
+        mIsMovingRight = isPressed;
+    }
 }
-
 void Game::render(){
     //renders game to the screen
     //clears window first
